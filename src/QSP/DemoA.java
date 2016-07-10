27a39c98f918451017a0307b8da93c54ea277ea8
shailasiva <shailaja.venkatarama@gmail.com>
2016-07-10 14:20:08 +0530

    Second time git push

---
+++ src/QSP/DemoA.java
@@ -0,0 +1,10 @@
+package QSP;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Trying GIT for the first time");
+	}
+
+}
