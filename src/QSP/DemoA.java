3ec19d0bad995c3e028590b9bda58cef110e12e8
shailasiva <shailaja.venkatarama@gmail.com>
2016-07-10 14:38:07 +0530

    Update DemoA.java

--- src/QSP/DemoA.java
+++ src/QSP/DemoA.java
@@ -6,6 +6,7 @@ public class DemoA {
 		// TODO Auto-generated method stub
    System.out.println("Trying GIT for the first time");
    System.out.println("Trying GIT for the try time");
+      System.out.println("Successful finally");
 	}
 
 }
